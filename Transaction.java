f3a78a8c42e0512614f5168ff3937105605cecdf
holladayad <amberholladay21@gmail.com>
2020-06-08 15:24:16 -0400

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,6 +1,23 @@
 package CIS484.Capstone;
 
+import java.util.*;
 
 public class Transaction {
     
+    private int transactionID;
+    private double transactionTotal;
+    private ArrayList<Items> transactionItems = new ArrayList<>();
+    public static int nextTransactionID = 0;
+    
+    public Transaction()
+    {
+        this.transactionID = nextTransactionID++;
+    }
+    
+    public void addItem(Items transactionItem)
+    {
+        // transactionTotal += transactionItem.getPrice();
+        transactionItems.add(transactionItem);
+        
+    }
 }
